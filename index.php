2ebef4edf89feb5d737a98d13f2c0a18cbdad555
= <=>
2023-06-22 11:03:44 +0100

    adding update

--- index.php
+++ index.php
@@ -17,11 +17,28 @@ td, th {
 tr:nth-child(even) {
   background-color: #dddddd;
 }
+
+.button {
+  background-color: #4CAF50;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
 </style>
 </head>
 <body>
 
-<h2>HTML Table</h2>
+<h2>Stok Management</h2>
+
+
+<a href="create.html" class="button">Create Article</a>
+
 
 <table>
   <tr>
